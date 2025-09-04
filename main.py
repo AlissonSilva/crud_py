4bf1022247e1976f05ef1142b2f13741ca38cb75
Alisson Oliveira <alisson_sao@hotmail.com>
2025-09-04 14:39:46 -0300

    Alteração na estrutura e criado uma rota para pasta view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import controller.usercontroller as user_controller
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
+import view.user as user_view
 
 app = FastAPI()
 
@@ -7,32 +7,4 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/users/")
-def create_user(username: str, email: str, name: str):
-    user = user_controller.criar_usuario(username, email, name)
-    return user
-
-@app.get("/users/")
-def list_users():
-    return user_controller.listar_usuarios()
-
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
-    user = user_controller.obter_usuario_por_id(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-@app.put("/users/{user_id}")
-def update_user_email(user_id: int, new_email: str): 
-    user = user_controller.atualizar_email(user_id, new_email)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    user = user_controller.deletar_usuario(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return {"detail": "User deleted"}
+app.include_router(user_view.router)
---
+++ view/user.py
@@ -0,0 +1,36 @@
+import controller.usercontroller as user_controller
+from fastapi import Depends, HTTPException,APIRouter
+
+#app = FastAPI()
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("/users/")
+def create_user(username: str, email: str, name: str):
+    user = user_controller.criar_usuario(username, email, name)
+    return user
+
+@router.get("/users/")
+def list_users():
+    return user_controller.listar_usuarios()
+
+@router.get("/users/{user_id}")
+def read_user(user_id: int):
+    user = user_controller.obter_usuario_por_id(user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.put("/users/{user_id}")
+def update_user_email(user_id: int, new_email: str): 
+    user = user_controller.atualizar_email(user_id, new_email)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    user = user_controller.deletar_usuario(user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"detail": "User deleted"}
