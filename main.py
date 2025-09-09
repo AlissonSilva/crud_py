c33c962b8c8bc35105392c3373495a221c32fafa
Alisson Oliveira <alisson_sao@hotmail.com>
2025-09-09 13:01:28 -0300

    Adicionado a função de carregar usuário padrão

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import view.user as user_view, view.auth as auth_view
 import view.category as category_view
 import view.product as product_view
 from sqlalchemy import create_engine 
-from util.util import Base, engine
+from util.util import Base, engine, SessionLocal
+from model.user import User
+import security.password_handler as pwd_handler
 
 
 Base.metadata.create_all(engine)
@@ -18,3 +20,19 @@ app.include_router(auth_view.router)
 app.include_router(user_view.router)
 app.include_router(category_view.router)
 app.include_router(product_view.router)
+
+def criar_usuario_padrao():
+    with SessionLocal() as sessao:
+        usuario = sessao.query(User).filter_by(username="admin").first()
+        if not usuario:
+            usuario = User(
+                username="admin",
+                name="Administrador",
+                email="admin@admin.com",
+                password=pwd_handler.hash_password("admin123")
+            )
+            sessao.add(usuario)
+            sessao.commit()
+            print("Usuário padrão criado: admin / admin123")
+
+criar_usuario_padrao()
